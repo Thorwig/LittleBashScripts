a073c26794020fd87018044ebb90da10a60df622
Thorwig <mouad.nossair@viacesi.fr>
2022-04-06 10:51:52 +0200

    Premier script

---
+++ fail2ban_resetlogs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#************************************************************
+# Sript qui permet de reset les logs et la base de donnée
+# de fail2ban
+# Script réalisé pour Charlie Solutions
+#************************************************************
+
+Database ="/var/lib/fail2ban/fail2ban.sqlite3"
+Logs="/var/log/fail2ban.log"
+
+function cleanup() {
+        sudo service fail2ban stop
+        sudo truncate -s 0 $Logs
+        sudo rm -rf $Database
+        sudo service fail2ban restart
+}
+
+cleanup
